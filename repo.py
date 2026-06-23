b76c686f860dfd20e1f375d46e615369906da983
Vijay Penguin <vijaypenguinbiotech@gmail.com>
2026-06-23 11:39:16 +0530

    Update application

--- repo.py
+++ repo.py
@@ -13,16 +13,7 @@ import re
 from fastapi import Header, HTTPException, Depends
 
 
-API_KEY =os.getenv('API_KEY')
 
-
-def verify_api_key(x_api_key: str = Header(None)):
-    if x_api_key != API_KEY:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid API Key"
-        )
-    return x_api_key
 
 
 
--- routers.py
+++ routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi import Depends
+
 from repo import (
     Balance_Sheet_Report,
     Cost_Center_Summary_Report,
@@ -32,12 +32,12 @@ router = APIRouter()
 
 
 @router.get("/tables")
-def list_tables(api_key: str = Depends(verify_api_key)):
+def list_tables():
     return list_database_tables_agent()
 
 
 @router.get("/tables/{table_name}/data")
-def get_table_data_endpoint(table_name: str, limit: int = 100,api_key: str = Depends(verify_api_key)):
+def get_table_data_endpoint(table_name: str, limit: int = 100):
     return get_table_data(table_name=table_name, limit=limit)
 
 
